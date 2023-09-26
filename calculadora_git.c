fc7bb421944a623b28ff113b6a408707de54fe85
Roldan Marquez Jorge Emmanuel <thebrock2004@gmail.com>
2023-09-25 19:50:46 -0600

    agregue switch

--- calculadora_git.c
+++ calculadora_git.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
 int main(void)
-
 {
+char operacion;
+
+printf("Ingresa operador: ");
+scanf("%c" , &operacion);
+
+switch (operacion){
+
+	case '+':
+        printf("Suma");
+	break;
+        case '-':
+        printf("Resta");
+        break;
+        case '*':
+        printf("Multiplicacion");
+        break;
+        case '/':
+        printf("Division");
+        break;
+        default:
+        printf("<<ERROR>>");
+}
   return 0;
 }
